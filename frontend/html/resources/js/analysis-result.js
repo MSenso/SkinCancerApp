8a9b60f037a099bf93f9e78e713daf79392adaf5
Maria Akhaladze <axaladzemaria@gmail.com>
2023-05-14 22:51:40 +0500

    added get method for analysis results

---
+++ frontend/html/resources/js/analysis-result.js
@@ -0,0 +1,29 @@
+const predictSessionId = sessionStorage.getItem("predict_session_id")
+const accessToken = sessionStorage.getItem("token")
+const fetchOptions = {
+    method: 'GET',
+    headers: {
+        'Authorization': 'Bearer ' + accessToken
+    }
+};
+fetch(`http://0.0.0.0:8001/predict_session/${predictSessionId}/predict`, fetchOptions)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        let resultStatus = document.getElementById("resultStatus");
+        let resultAction = document.getElementById("resultAction");
+
+        if (data.status_name === "здоров" || data.status_name === "неизвестен")
+            resultStatus.innerHTML += `С вероятностью ` + data.predict_score + ` вы здоровы`;
+        else
+            resultStatus.innerHTML += `С вероятностью ` +  data.predict_score + ` у вас ` + data.status_name;
+        resultAction.innerHTML = `<p class="font-weight-bold">Важно!</p>
+    <p>Этот инструмент предназначен для пред-диагностики и не может служить основанием для постановки диагноза.</p>
+    <p>В случае опасения насчёт того или иного заболевания рекомендуем вам обратиться к специалисту</p>
+    <a class="btn btn-outline-dark btn-rounded" href="http://0.0.0.0:3001/doctors">Просмотреть врачей</a>`;
+    })
+    .catch(error => {
+        // Handle the error
+        console.error(error);
+        responseElement.innerText = "Произошла ошибка при загрузке результата. Попробуйте позднее";
+    });
